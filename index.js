1eedfb2c3d3a7669adb1f86c1129c8f05cca0bb3
Higor Diego <higordiegoti@gmail.com>
2020-01-18 16:09:43 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+const app = express()
+
+app.use(express.json())
+
+const Angularjs = mongoose.model('angularjs', {
+    name: { type: String, required: true },
+    email: { type: String, required: true, index: { unique: true } },
+    age: { type: String, required: true },
+});
+
+
+app.post('/', async (req, res) => {
+    try {
+        if (req.body) await Angularjs.create(req.body)
+        res.status(201).json()
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+
+app.get('/', async (_, res) => {
+    try {
+        const result = await Angularjs.find()
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+app.get('/:_id', async (_, res) => {
+    try {
+        const result = await Angularjs.find(req.params)
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+app.put('/:_id', async (_, res) => {
+    try {
+        const result = await Angularjs.update(req.params, req.body)
+        res.status(200).json()
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+app.delete('/:_id', async (_, res) => {
+    try {
+        await Angularjs.remove(req.params)
+        res.status(200).json()
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+app.listen(3000, () => console.log('Flying angularJS... : )'))
